b5e1451a0a415fe23f63e01f52e28ba7219d06da
Matthew Turner <maturner01@gmail.com>
2019-02-21 15:15:29 -0800

    finished updates for HW2 and used to grade

--- source/app.d
+++ source/app.d
@@ -2,19 +2,22 @@ module app;
 
 import std.getopt;
 
+import std.algorithm : filter, joiner, map, sort, sum, uniq;
 import std.array : array, replace, split;
-import std.algorithm : joiner, map, sum;
+import std.ascii : lowercase;
 import std.conv : to;
 import std.file : dirEntries, readText, SpanMode, write;
 import std.json : parseJSON, JSONValue;
-import std.path : baseName, chainPath, globMatch;
+import std.path : baseName, chainPath, extension, globMatch;
 import std.process : spawnShell;
 import std.range : chunks, repeat;
+import std.regex : matchFirst;
 import std.stdio : writeln;
 
 import dli;
 
 import services : StudentLookup;
+
 
 int main(string[] args)
 {
@@ -64,27 +67,36 @@ void runApp(string courseId, string assignmentId, string questionsFile, bool ope
             .array;
     
     // Load each student's submission.
-    /* Submission[] studentSubmissions = submissionsDir.loadSubmissions; */
     // XXX  NEED TO ADD THIS AS A CLI OPTION OR GLOB OVER DOCX AND PDF AND
     // XXX  WE'LL NEED TO HANDLE NETLOGO ON THE NEXT HW; doin that next refactor
-    Submission[] studentSubmissions = submissionsDir.loadSubmissions("*.pdf");
+    /* Submission[] studentSubmissions = submissionsDir.loadSubmissions("*.pdf"); */
+    Submission[] studentSubmissions = submissionsDir.loadSubmissions;
 
     // Run the evaluation method on each submission, using the questionSet
     // as the source of solutions.
-    Evaluation[] studentEvaluations = 
-        studentSubmissions  //[0..1]
-            .map!(a => a.evaluateSubmission(questionSet, openFile))
-            .array;
-
+    /* Evaluation[] studentEvaluations = */ 
     // Specify directory to which we write evaluation PDFs.
     string evaluationDir = 
         chainPath("homework", "evaluations", courseId, assignmentId)
             .array;
 
-    // Write each student's evaluation to the evaluation directory.
-    foreach (eval; studentEvaluations)
-        eval.writeReport(evaluationDir, 1);
+    // Extract homework index from questions file name, eg HW3.json -> 3.
+    size_t homeworkIndex = 
+        questionsFile
+            .matchFirst(r".*(\d).*")[1]
+            .to!size_t;
+
+    Evaluation[] studentEvaluations;
+    foreach (sub; studentSubmissions)
+    {
+        Evaluation eval = sub.evaluateSubmission(questionSet, openFile);
+
+        studentEvaluations ~= eval;
+
+        eval.writeReport(evaluationDir, homeworkIndex);
+    }
 }
+
 
 bool _checkPartScore(float partScore, float maxPartScore) 
 {
@@ -199,10 +211,22 @@ Evaluation of COGS 122 HW ` ~ homeworkNumber.to!string ~ `} \\[1em]
 
             // Keep track of which question we are on.
             size_t qIdx = 1;
-
             // Iterate over questions, extract and grade parts (see below).
-            foreach (string questionTitle, float[] partScores; questionScores)
+
+            // We need to order the titles by the index given in JSON,
+            // which is according to the PDF version given to students.
+            string[] orderedTitles = 
+                questionScores
+                    .keys
+                    .sort!( (a, b) =>
+                        questionSet[a]["index"].to!string.to!size_t 
+                        < questionSet[b]["index"].to!string.to!size_t
+                    )
+                    .array;
+                            
+            foreach (string questionTitle; orderedTitles)
             {
+                float[] partScores = questionScores[questionTitle];
                 // Extract the number of points possible for each qn part.
                 string[] partPointsPossible = 
                     questionSet[questionTitle]["parts"]
@@ -213,14 +237,33 @@ Evaluation of COGS 122 HW ` ~ homeworkNumber.to!string ~ `} \\[1em]
                         .map!(a => a["Points possible"].to!string)
                         .array;
 
+                // Use letters for question parts. Re-initialize for each
+                // new question.
+                auto partLetters = lowercase.map!(
+                    letter => "(" ~ letter.to!string ~ ")"
+                );
+                
+
                 // Iterate over each score in the question's part, w/ index.
                 foreach (partIdx, partScore; partScores)
                 {
-                    string questionPartIndex = qIdx.to!string ~ "." 
-                                               ~ (partIdx + 1).to!string;
+                    // Get the current letter and move to the next, starting
+                    // from "(a)".
+                    string thisLetter = partLetters.front;
+                    partLetters.popFront;
+
+                    string questionPartIndex = qIdx.to!string ~ " " ~ thisLetter;
+
                     // Make question part bold face if start of new question.
                     if (partIdx == 0)
-                        questionPartIndex = `{\large \bf` ~ questionPartIndex ~ `}`;
+                    {
+                        questionPartIndex = 
+                            `{{\small` 
+                            ~ questionSet[questionTitle]["Title"].to!string 
+                            ~ `} \bf ` 
+                            ~ questionPartIndex.replace(`"`, "")
+                            ~ `}`;
+                    }
 
                     // Append new tex source to the existing tex source table.
                     texTable ~=  "\t" ~
@@ -255,8 +298,9 @@ Evaluation of COGS 122 HW ` ~ homeworkNumber.to!string ~ `} \\[1em]
                 totalPointsPossible += 
                     questionSet[questionTitle]["parts"]
                         .array
-                        .map!(a => a["Points possible"].integer.to!float)
-                        .array
+                        // Weird way to get JSON to read these as floats.
+                        // I tried everything I could think of better than this.
+                        .map!(a => a["Points possible"].to!string.to!float)
                         .sum;
             }
 
@@ -274,22 +318,74 @@ Evaluation of COGS 122 HW ` ~ homeworkNumber.to!string ~ `} \\[1em]
 }
 
 
-Submission[] loadSubmissions(string submissionsDir, 
-                             string extension="*.docx")
+string _getStudentFromFilename(string fileName)
+{
+    return baseName(fileName).replace("late_","").split("_")[1];
+}
+
+
+string[] _getStudentsFromFilenames(string[] submissionFiles)
+{
+    return submissionFiles.map!(a => _getStudentFromFilename(a)).array;
+}
+
+
+Submission[] loadSubmissions(string submissionsDir)
 {
     string[] submissionFiles = 
 
         dirEntries(
             submissionsDir, 
-            extension, 
+            "*", 
             SpanMode.shallow
         )
         .map!"a.to!string"
         .array;
 
-    return submissionFiles
-            .map!(a => new Submission(a))
+    string[] uniqueStudentIds = 
+        _getStudentsFromFilenames(submissionFiles)
+            .sort
+            .uniq
             .array;
+    
+    // If there are more files than students, then there are multiple 
+    // assignments per student. Handle this case if necessary, or just
+    // create a submission for every submission file if there is a one-to-one
+    // file-to-student relationship.
+    if (submissionFiles.length > uniqueStudentIds.length)
+    {
+        /* baseName(fp).replace("late_","").split("_")[0..2]; */
+        // We need to organize the files into arrays by studentId, which I
+        // am keeping a string to make it easy in case they put letters in
+        // there sometime. Probably won't. But no need to optimize performance
+        // in this application.
+        string[][] filesByStudent;
+        foreach (studentId; uniqueStudentIds)
+        {
+            filesByStudent ~= 
+                submissionFiles
+                    .filter!(
+                        fileName => _getStudentFromFilename(fileName) == studentId
+                    )
+                    .array;
+                            
+        }
+
+        // Build list of submissions, each from a list of student files that
+        // go with the assignment. 
+        Submission[] ret;
+        foreach (studentFiles; filesByStudent)
+        {
+            ret ~= new Submission(studentFiles);
+        }
+
+        return ret;
+    }   
+    else
+
+        return submissionFiles
+                .map!(a => new Submission(a))
+                .array;
 }
 
 
@@ -304,8 +400,19 @@ Submission[] loadSubmissions(string submissionsDir,
  */
 class Submission 
 { 
-    string filePath, studentName, studentId;
+    // Each student, as identified by their name and ID...
+    string studentName, studentId;
+    // submitted their homework, which we evaluate. The 
+    // Evaluation of the submission is an attribute of the submission.
     Evaluation evaluation;
+
+    // Not used if the string constructor is used, ie one files 
+    // (PDF/docx, .nlogo, etc.) per student.
+    string[] filePaths;
+
+    // Not used if string[] constructor is used, 
+    // ie many assignments per student.
+    string filePath;
 
     public:
     /**
@@ -330,6 +437,15 @@ class Submission
         string[2] name_id = baseName(fp).replace("late_","").split("_")[0..2];
         studentName = name_id[0];
         studentId = name_id[1];
+    }
+
+    /// Some homeworks have more than one 
+    this(string[] fps)
+    {
+        string[2] name_id = baseName(fps[0]).replace("late_","").split("_")[0..2];
+        studentName = name_id[0];
+        studentId = name_id[1];
+        filePaths = fps;
     }
 
     /**
@@ -357,9 +473,18 @@ class Submission
             // Open the file, giving user some info on what's going on.
             if (openFile && qIdx == 0)
             {
-                writeln("File path:");
-                writeln(filePath);
-                openFileExternally(filePath);
+                if (filePaths.length > 0)
+                {
+                    writeln("File paths:");
+                    writeln(filePath);
+                    openFileExternally(filePaths);
+                }
+                else
+                {
+                    writeln("File path:");
+                    writeln(filePath);
+                    openFileExternally(filePath);
+                }
             }
             
             // A question has multiple parts; iterate over these parts.
@@ -386,14 +511,41 @@ class Submission
 
     private:
 
-    /// Open the file associated with a part. Tell user what command was issued.
+    // Open one file associated with a homework. 
     void openFileExternally(string filePath)
     {
-        string cmd = `/usr/bin/open "` ~ filePath ~ `"`;
-        writeln("Shell command to open submission:");
-        writeln(cmd);
-        spawnShell(cmd);
+        openFileExternally([filePath]);
+    }
+
+    // Open one or many files using the default program. 
+    // Tell user what command was issued.
+    void openFileExternally(string[] filePaths)
+    {
+        // Filter out annoying Word temporary backup files.
+        filePaths = filePaths.filter!(fp => baseName(fp)[0].to!string != "~").array;
+
+        // The command will vary depending on the file extension. See below.
+        string cmd;
+        foreach (filePath; filePaths)
+        {
+            writeln("\nShell command to open submission:");
+            // If it's a NetLogo file we need a specialized `open` command.
+            if (extension(filePath) == ".nlogo")
+            {
+                cmd = `/usr/bin/open -na "/Applications/NetLogo 6.0.app" "` ~ filePath ~ `"`;  
+            }
+            // Otherwise, use the general `open` command.
+            else
+            {
+                // Construct the properly-quoted shell command to open the file.
+                cmd = `/usr/bin/open "` ~ filePath ~ `"`;
+            }
+            cmd.writeln;
+
+            // Execute the open command.
+            cmd.spawnShell;
         }
+    }
 }
 
 
@@ -468,9 +620,15 @@ class QuestionSet
         questionsJson = parseJSON(questionsString);    
 
         questionTitles = 
-            questionsJson.array
-                         .map!(a => a["Title"].to!string.replace("\"", ""))
-                         .array;    
+            questionsJson
+                .array
+                .sort!(
+                    (a, b) => 
+                        a["index"].to!string.to!size_t 
+                        < b["index"].to!string.to!size_t
+                )
+                .map!(a => a["Title"].to!string.replace("\"", ""))
+                .array;    
 
         foreach (idx, questionTitle; questionTitles)
             titleIndexLookup[questionTitle] = idx;
